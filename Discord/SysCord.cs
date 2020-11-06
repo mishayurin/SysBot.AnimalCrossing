08429a953e06e1658881e79a68902c6ef32f59b2
mishayurin <mishayurin@icloud.com>
2020-11-06 18:03:53 +0300

    Update SysCord.cs

--- Discord/SysCord.cs
+++ Discord/SysCord.cs
@@ -135,8 +135,8 @@ namespace SysBot.AnimalCrossing
                 return;
 
             // We don't want the bot to respond to itself or other bots.
-            if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot)
-                return;
+            //if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot)
+            //    return;
 
             // Create a number to track where the prefix ends and the command begins
             int pos = 0;
